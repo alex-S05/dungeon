c8e5c6ed22861da2acfb01e8e0a3918973b309a0
Alex Stocker <alex.stock05@hotmail.com>
2023-07-10 16:43:07 +0100

    Added select player feature.

--- main.cpp
+++ main.cpp
@@ -15,23 +15,30 @@ int dungeon[SIZE][SIZE] = {
     {0,0,0,0,0,0,0,0,0},
 };
 
-void printDungeon() {
+void printDungeon(char p) {
     for (int i = 0; i < SIZE; i++) {
         cout << '\n';
         // dungeon[i] == {0,0,0,0,0,0,0,0,0}
         for (int j = 0; j < SIZE; j++) {
             // dungeon[i][j] == 0
-            cout << dungeon[i][j] << ' ';
+            if (i == 0  && j == 0) {
+                cout << p << ' ';
+            } else {
+                cout << dungeon[i][j] << ' ';
+            }
+            
         }
     }
     cout << '\n';
     
 }
 
-
 int main() {
+    char player;
+    cout << "Enter a charcter to represent the player: ";
+    cin >> player;
 
-    printDungeon();
+    printDungeon(player);
 
 
     return 0;
